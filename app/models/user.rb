7901838bb4498596cc09f47be9873cd5eb5a4958
Veenus Gupta <smartveenus@gmail.com>
2013-10-28 11:41:23 -0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,13 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean          default(FALSE)
 #
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
+  has_many :microposts, dependent: :destroy
   has_secure_password
 
   before_save { email.downcase! }
@@ -25,6 +28,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   after_validation { self.errors.messages.delete(:password_digest) }
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
